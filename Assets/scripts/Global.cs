6a4049e54cee66b74030ae960284f9ca154fafe9
MoophHD <d_sizykh@mail.ru>
2017-10-22 20:22:06 +0300

    setup, flexible bg, global singleton

---
+++ Assets/scripts/Global.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour {
+	private static Global _instance;
+	public static Global Instance {
+	get {
+		if (_instance == null) {
+			GameObject go = new GameObject("Global");
+			go.AddComponent<Global>();
+		}
+
+		return _instance;
+	}
+	}
+	public Vector2 MaxCameraBounds {
+		get {
+			return Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+		}
+	}
+
+	public Vector2 MinCameraBounds {
+		get {
+			return Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f));
+		}
+	}
+
+	void Awake() {
+		_instance = this;
+	}
+}
---
+++ Assets/scripts/secondary/fixSizeBg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class fixSizeBg : MonoBehaviour {
+	private GameObject obj;
+	private SpriteRenderer rend;
+	
+
+	void Awake() {
+		if (GetComponent<Transform>().localScale != new Vector3(1f,1f,1f) ) return; // called only once
+		obj = this.gameObject;
+		rend = obj.GetComponent<SpriteRenderer>();
+		float rendX = rend.bounds.max.x;
+		float cameraX = Global.Instance.MaxCameraBounds.x;
+		float cameraY = Global.Instance.MaxCameraBounds.y;
+		float rendY = rend.bounds.max.y;
+
+		obj.GetComponent<Transform>().localScale = new Vector3(cameraX / rendX, cameraY / rendY, 1f); 
+	}
+}
