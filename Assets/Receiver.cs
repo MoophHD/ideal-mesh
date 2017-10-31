7fb351d7ee40420cc4d8ba6c9d466b9f5fc69145
MoophHD <d_sizykh@mail.ru>
2017-10-31 10:55:32 +0300

    line + distance joint tests

---
+++ Assets/Receiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receiver : MonoBehaviour {
+	public GameObject obj1;
+	public GameObject obj2;
+	
+	public void AddDist() {
+		obj1.GetComponent<DistanceJoint2D>().distance += 0.5f;
+	}
+
+	public void RmDist() {
+		obj1.GetComponent<DistanceJoint2D>().distance -= 0.5f;
+	}
+}
